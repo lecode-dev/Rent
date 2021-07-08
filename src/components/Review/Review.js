a6e627bdba773939f2acbd89721f4b612ae3b2f8
karenmscg <karen.myllena@gmail.com>
2021-07-08 18:59:39 -0300

    Almost done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,6 +4,7 @@ import image_thomas from "../../images/image_thomas.svg";
 function Review() {
   return (
     <div className={classes.thomasContainer}>
+        <div className = {classes.thomasImage} />
       <h3 className = {classes.thomasTitle}>Thomas Schinyder</h3>
       <p className = {classes.thomasText}>Definitely the best place to get your dream car and hit the road.</p>
       <img src={image_thomas} className={classes.thomas} />
